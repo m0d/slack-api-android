fd4618e17c4a8c095a8831ac7b755b66e96a1bab
Gregory Pawelczuk <grzegorz.pawelczuk@nftlearning.com>
2018-02-22 09:23:59 +0100

    im.history && users.info ++

--- slack-api/src/main/java/pl/hellsoft/slack/wrapper/SlackApiConstants.kt
+++ slack-api/src/main/java/pl/hellsoft/slack/wrapper/SlackApiConstants.kt
@@ -1,5 +1,18 @@
 package pl.hellsoft.slack.wrapper
 
+/**
+ * @author Maciej Madetko
+ * @email maciej.madetko@ftlearning.com
+ * Nikkei FT Learning Limited
+ * @since 22/01/2018.
+ *
+ * @author Grzegorz Pawełczuk
+ * @email grzegorz.pawelczuk@ftlearning.com
+ * Nikkei FT Learning Limited
+ * @since 22/02/2018
+ */
+
+
 class SlackApiConstants {
     companion object {
         const val SLACK_WEB_API_URL = "https://slack.com/api"
@@ -12,6 +25,12 @@ class SlackApiConstants {
 
         // channels
         const val CHANNELS_HISTORY = "channels.history"
+
+        //IM
+        const val IM_HISTORY = "im.history"
+
+        //Users
+        const val USERS_INFO = "users.info"
 
         // chat
         const val CHAT_ME_MESSAGE = "chat.meMessage"
--- slack-api/src/main/java/pl/hellsoft/slack/wrapper/webapi/WrapperApiInterface.kt
+++ slack-api/src/main/java/pl/hellsoft/slack/wrapper/webapi/WrapperApiInterface.kt
@@ -5,16 +5,25 @@ import pl.hellsoft.slack.wrapper.webapi.model.BotInfoResponse
 import pl.hellsoft.slack.wrapper.webapi.model.MeMessageResponse
 import pl.hellsoft.slack.wrapper.webapi.model.PostMessageResponse
 import io.reactivex.Observable
+import pl.hellsoft.slack.wrapper.webapi.model.UsersInfoResponse
 
 /**
  * @author Maciej Madetko
  * @email maciej.madetko@ftlearning.com
  * Nikkei FT Learning Limited
  * @since 19/01/2018.
+ *
+ * @author Grzegorz Pawełczuk
+ * @email grzegorz.pawelczuk@ftlearning.com
+ * @desc getImHistoryEvents impl.
+ * @since 22/02/2018
  */
+
 interface WrapperApiInterface {
     fun postMessage(channel: String, message: String) : Observable<PostMessageResponse>
     fun meMessage(channel: String, message: String) : Observable<MeMessageResponse>
     fun getChannelHistoryEvents(channel: String, latest: String?, oldest: String?, inclusive: Boolean?, count: Int, unreads: Boolean?) : Observable<HistoryResponse>
+    fun getImHistoryEvents(channel: String, latest: String?, oldest: String?, inclusive: Boolean?, count: Int, unreads: Boolean?) : Observable<HistoryResponse>
     fun getBotInfo(bot: String) : Observable<BotInfoResponse>
+    fun getUserInfo(userId: String) : Observable<UsersInfoResponse>
 }
